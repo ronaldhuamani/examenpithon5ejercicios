d1547929888fb0f797914b66ce101225992bcfd7
ronaldhuamani <fredyronaldhuamani@gmail.com>
2020-10-11 15:18:00 +0000

    examen4

---
+++ examen/examen4.py
@@ -0,0 +1,14 @@
+def definirsigno_correspondiente():
+    #datos de entrada
+    operacion_suma=input("presiona la funcion 1 para saber las siguientes operaciones: ")
+    #proceso
+    operacion_suma =input("ingrese la opcion suma y te dare un ejemplo: ")
+    print ("2+9=11, "+operacion_suma)
+    operacion_ejemplo_resta =input("ingrese la opcion resta y te dare un ejemplo: ")
+    print ("10-2=8, "+operacion_ejemplo_resta)
+    operacion_ejemplo_potencia =input("ingrese la opcion potencia y te dare un ejemplo: ")
+    print ("20/2=10, "+operacion_ejemplo_potencia)
+    #datos de salida
+    print("son las operaciones aritmeticas correctas")
+
+definirsigno_correspondiente()
