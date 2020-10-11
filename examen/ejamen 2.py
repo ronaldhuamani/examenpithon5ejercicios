c98f8419147c86b1dc6c55ed9b752e87b12b8524
ronaldhuamani <fredyronaldhuamani@gmail.com>
2020-10-11 02:39:02 +0000

    examen2

---
+++ examen/ejamen 2.py
@@ -0,0 +1,24 @@
+def calcularpuntos():
+    #datos de entrada
+    print('desempeño de los profesores:')
+    print()
+    print("""
+    Menu: Seleccione una opcion
+    1. puntos realizados de >50<100
+    2. puntos realizados de >101<150
+    3. puntos realizados de >151
+    """)
+    menu = int(input())
+    #proceso
+    if menu == 1:
+        print("salario es igual a 10%:")
+    elif menu == 2:
+        print("salario es igual a 50%:")
+    elif menu == 3:
+        print("salario es igual a 80%:")
+    else:
+        print('Ingrese una opcion correcta.')
+    #datos de salida
+    print("es el monto que percibira el profesor")
+
+calcularpuntos()
