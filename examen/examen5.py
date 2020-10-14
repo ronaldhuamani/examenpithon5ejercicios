48bb72ffca775e9604cff4978f3afb8d457fce32
ronaldhuamani <fredyronaldhuamani@gmail.com>
2020-10-14 15:37:54 +0000

    examen 5

---
+++ examen/examen5.py
@@ -0,0 +1,13 @@
+def calcularSalarioFRHH():
+    #datos de entrada
+    str
+    print("ingrese el 10% del salario ganado por cada año: ")
+    salario=1400
+    #proceso
+    for i in [1,2,3,4,5,6,7]:
+     print(f"En el año {i} y el salario de 10% es: {salario*100/100: .2f}")
+     salario=salario*1.1
+    #datos de salida
+    print("son los los salarios ganados por cada año")
+
+calcularSalarioFRHH()
